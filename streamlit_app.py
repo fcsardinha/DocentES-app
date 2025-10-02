da06164313159fb5d8856c1a14c8d3b2ba718ac7
Farley C Sardinha <fcsardinha@gmail.com>
2025-10-02 19:13:39 +0000

    Inserindo dados

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,13 @@ st.write(
     "Bem-vindo à DocentES, a plataforma sobre os Docentes do Espírito Santo!"
 )
 
+# Carregar dataset
+@st.cache_data
+def carregar_dados(name):
+    return pd.read_csv(name, sep=";")
+
+df_etapa = carregar_dados("docentes_etapas.csv")
+
+# Mostrar dados
+st.subheader("🔍 Visualização da Tabela de Dados")
+st.dataframe(df_etapa)
